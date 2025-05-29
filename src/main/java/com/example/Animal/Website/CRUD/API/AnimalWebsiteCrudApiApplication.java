8347a3c5759643ea054946c4f00f143ecde1e938
Antoine Wimberly <a_wimberly@uncg.edu>
2025-05-29 19:18:40 -0400

    commit

---
+++ src/main/java/com/example/Animal/Website/CRUD/API/AnimalWebsiteCrudApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.Animal.Website.CRUD.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalWebsiteCrudApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalWebsiteCrudApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Animal/Website/CRUD/API/AnimalWebsiteCrudApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Animal.Website.CRUD.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalWebsiteCrudApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
