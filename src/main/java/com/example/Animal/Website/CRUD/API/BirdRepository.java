04bf3a5aec40e582109c75c487a228e88493627c
Antoine Wimberly <a_wimberly@uncg.edu>
2025-05-31 15:34:34 -0400

    Updated BirdService.java

--- src/main/java/com/example/Animal/Website/CRUD/API/BirdRepository.java
+++ src/main/java/com/example/Animal/Website/CRUD/API/BirdRepository.java
@@ -1,7 +1,9 @@
 package com.example.Animal.Website.CRUD.API;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BirdRepository {
+public interface BirdRepository extends JpaRepository<Bird, Long> {
+    
 }
--- src/main/java/com/example/Animal/Website/CRUD/API/BirdService.java
+++ src/main/java/com/example/Animal/Website/CRUD/API/BirdService.java
@@ -1,8 +1,67 @@
 package com.example.Animal.Website.CRUD.API;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
 public class BirdService {
-    
+
+    @Autowired
+    private BirdRepository birdRepository;
+
+    public Object getAllBirds() {
+        return birdRepository.findAll();
+}
+
+    public Bird getBirdById(@PathVariable Long birdId) {
+        return birdRepository.findById(birdId).orElse(null);
+    }
+
+    public Object getBirdByName(String name) {
+        return birdRepository.getBirdByName(name);
+    }
+
+    public Object getBirdByDescription(String description) {
+        return birdRepository.getBirdByDescription(description);
+    }
+
+    public Bird getBirdByBreed(String breed) {
+        return birdRepository.getBirdByBreed(breed);
+    }
+
+    public Bird getBirdByAge(double age) {
+        return birdRepository.getBirdByAge(age);
+    }
+
+    public Bird addBird(Bird bird) {
+        return birdRepository.save(bird);
+    }
+
+    public String writeJson(Bird bird) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            objectMapper.writeValue(new File("birds.json"), bird);
+            return "Bird written to JSON file successfully";
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "Error writing bird to JSON file";
+        }
+    }
+
+    public Bird readJson() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            return objectMapper.readValue(new File("birds.json"), Bird.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
 }
