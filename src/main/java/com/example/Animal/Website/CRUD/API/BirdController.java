f0a487287cd071bb53887a65b2e91c545533d433
Antoine Wimberly <a_wimberly@uncg.edu>
2025-06-05 19:50:20 -0400

    Convert to MVC

--- src/main/java/com/example/Animal/Website/CRUD/API/BirdController.java
+++ src/main/java/com/example/Animal/Website/CRUD/API/BirdController.java
@@ -1,74 +1,62 @@
 package com.example.Animal.Website.CRUD.API;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
+import org.springframework.ui.Model;
+
+@Controller
+@RequestMapping("/birds")
 public class BirdController {
 
     @Autowired
     private BirdService birdService;
 
-    @GetMapping("/birds")   
-    public Object getAllBirds() {
-        return birdService.getAllBirds();
+    @GetMapping
+    public Object getAllBirds(Model model) {
+        model.addAttribute("birdslist", birdService.getAllBirds());
+        return "bird-list";
     }
 
-    @GetMapping("/birds/{birdId}")
-    public Bird getBirdById(@PathVariable Long birdId) {
-        return birdService.getBirdById(birdId);
+    @GetMapping("/{birdId}")
+    public Object getBirdById(@PathVariable Long birdId, Model model) {
+        model.addAttribute("bird", birdService.getBirdById(birdId));
+        model.addAttribute("Title", "Bird #" + birdId);
+        return "bird-details";
     }
 
-    @GetMapping("/birds/name/{name}")
-    public Object getBirdByName(@PathVariable String name) {    
-        return birdService.getBirdByName(name);
+    @GetMapping("/new")
+    public Object showCreateForm(Model model) {
+        model.addAttribute("bird", new Bird());
+        return "bird-create";
     }
 
-    @GetMapping("/birds/description/{description}")
-    public Object getBirdByDescription(@PathVariable String description) {
-        return birdService.getBirdByDescription(description);
+    @GetMapping("/edit/{birdId}")
+    public Object showUpdateForm(@PathVariable Long birdId, Model model) {
+        model.addAttribute("bird", birdService.getBirdById(birdId));
+        return "bird-update";
     }
 
-    @GetMapping("/birds/breed/{breed}")
-    public Object getBirdByBreed(@PathVariable String breed) {
-        return birdService.getBirdByBreed(breed);
+    @PostMapping("/new")
+    public Object createBird(@ModelAttribute Bird bird) {
+        birdService.addBird(bird);
+        return "redirect:/birds";
     }
 
-    @GetMapping("/birds/age/{age}")
-    public Object getBirdByAge(@PathVariable int age) {
-        return birdService.getBirdByAge(age);
+    @PostMapping("/update")
+    public Object updateBird(@ModelAttribute Bird bird) {
+        birdService.updateBird(bird.getBirdId(), bird);
+        return "redirect:/birds/" + bird.getBirdId();
     }
 
-    @PostMapping("/birds")
-    public Object addBird(@RequestBody Bird bird) {
-        return birdService.addBird(bird);
-    }
-
-    @PutMapping("/birds/{birdId}")
-    public Bird updateBird(@PathVariable Long birdId, @RequestBody Bird bird) {
-        return birdService.updateBird(birdId, bird);
-    }
-
-    @DeleteMapping("/birds/{birdId}")
+    @GetMapping("/delete/{birdId}")
     public Object deleteBird(@PathVariable Long birdId) {
         birdService.deleteBird(birdId);
-        return birdService.getAllBirds();
-    }
-
-    @PostMapping("/birds/write")    
-    public Object writeJson(@RequestBody Bird bird) {
-        birdService.writeJson(bird);
-        return birdService.writeJson(bird);
-    }
-
-    @GetMapping("/birds/read")
-    public Object readJson() {
-        return birdService.readJson();
+        return "redirect:/birds";
     }  
 }
